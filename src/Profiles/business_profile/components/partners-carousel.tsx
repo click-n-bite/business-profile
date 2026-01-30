5defbd69991a81fe73d96e2b458039f0a47ec20f
souzan12 <clicknbite.menu@gmail.com>
2026-01-30 23:20:09 +0200

    arabic version

--- src/Profiles/business_profile/components/partners-carousel.tsx
+++ src/Profiles/business_profile/components/partners-carousel.tsx
@@ -13,22 +13,22 @@ export function PartnersCarousel({ partners }: { partners: BusinessPartner[] }) 
 		speed: 500,
 		autoplay: true,
 		autoplaySpeed: 5000,
-		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToShow: 2,
+		slidesToScroll: 2,
 		arrows: false,
 		responsive: [
 			{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 4
+					slidesToShow: 3,
+					slidesToScroll: 3
 				}
 			},
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3
+					slidesToShow: 2,
+					slidesToScroll: 2
 				}
 			},
 			{
