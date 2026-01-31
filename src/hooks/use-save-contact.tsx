2c975e3778bd780131f0c566b0a0bd3c0050508a
souzan12 <clicknbite.menu@gmail.com>
2026-01-31 23:27:00 +0200

    adding services

--- src/hooks/use-save-contact.tsx
+++ src/hooks/use-save-contact.tsx
@@ -1,3 +1,32 @@
+// "use client"
+
+// import { useState } from "react"
+// import { createContactVCard } from "@/utils/contact-utils"
+// import ContactSavedModal from "@/components/tenant/tenant-contact-modal"
+
+// export const useSaveContact = () => {
+// 	const [modalOpen, setModalOpen] = useState(false)
+
+// 	const [savedName, setSavedName] = useState("")
+
+// 	const saveContact = (name: string, phoneNumber: string): void => {
+// 		createContactVCard(name, phoneNumber)
+
+// 		setSavedName(name)
+// 		setModalOpen(true)
+// 	}
+
+// 	const ContactModal = () => (
+// 		<ContactSavedModal isOpen={modalOpen} onClose={() => setModalOpen(false)} name={savedName} />
+// 	)
+
+// 	return {
+// 		saveContact,
+// 		ContactModal,
+// 		isModalOpen: modalOpen,
+// 		setModalOpen
+// 	}
+// }
 "use client"
 
 import { useState } from "react"
@@ -7,23 +36,37 @@ import ContactSavedModal from "@/components/tenant/tenant-contact-modal"
 export const useSaveContact = () => {
 	const [modalOpen, setModalOpen] = useState(false)
 
-	const [savedName, setSavedName] = useState("")
+	const [contactData, setContactData] = useState({ name: "", phone: "" })
 
-	const saveContact = (name: string, phoneNumber: string): void => {
-		createContactVCard(name, phoneNumber)
-
-		setSavedName(name)
+	const showSaveModal = (name: string, phone: string) => {
+		setContactData({ name, phone })
 		setModalOpen(true)
 	}
 
-	const ContactModal = () => (
-		<ContactSavedModal isOpen={modalOpen} onClose={() => setModalOpen(false)} name={savedName} />
-	)
+	const saveContact = () => {
+		const { name, phone } = contactData
+
+		createContactVCard(name, phone)
+		setModalOpen(false)
+	}
+
+	const closeModal = () => {
+		setModalOpen(false)
+		setContactData({ name: "", phone: "" })
+	}
 
 	return {
-		saveContact,
-		ContactModal,
-		isModalOpen: modalOpen,
-		setModalOpen
+		saveContact: showSaveModal,
+
+		ContactModal: () => (
+			<ContactSavedModal
+				isOpen={modalOpen}
+				onClose={closeModal}
+				onSave={saveContact}
+				name={contactData.name}
+				phone={contactData.phone}
+			/>
+		),
+		isModalOpen: modalOpen
 	}
 }
